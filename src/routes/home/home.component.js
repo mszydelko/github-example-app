f76d69031120fb59b196896253fdf85bc72ebe36
Gosia <m_szydelko@yahoo.pl>
2018-04-21 08:50:51 +0200

    app init

---
+++ src/routes/home/home.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+        <p className="App-intro">
+            New App
+        </p>
+        </div>
+    );
+    }
+}
+
+export default Home;
